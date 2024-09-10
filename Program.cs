c542e488ea257b65d6ea4edb5cc4d8bf490afb51
makyo222 <145643799+makyo222@users.noreply.github.com>
2024-09-10 10:06:50 +0200

    2.feladat kész

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Program t = new Program();
             t.GetData("meccs.txt");
+            t.Feladat2();
         }
         public List<foci> meccsek = new();
         public void GetData(string fileName)
@@ -13,16 +14,28 @@
             foreach(var line in File.ReadAllLines(fileName).Skip(1))
             {
                 int f = int.Parse(line.Split(" ")[0]);
-                int hg = int.Parse(line.Split(" ")[0]);
-                int vg = int.Parse(line.Split(" ")[0]);
-                int hf = int.Parse(line.Split(" ")[0]);
-                int vf = int.Parse(line.Split(" ")[0]);
+                int hg = int.Parse(line.Split(" ")[1]);
+                int vg = int.Parse(line.Split(" ")[2]);
+                int hf = int.Parse(line.Split(" ")[3]);
+                int vf = int.Parse(line.Split(" ")[4]);
                 string h = line.Split(" ")[5];
                 string v = line.Split(" ")[6];
 
                 meccsek.Add(new foci(f, hg, vg, hf, vf, h, v));
             }
             
+        }
+        public void Feladat2()
+        {
+            Console.Write("Írja be a forduló számát:");
+            int fordulo = Convert.ToInt32(Console.ReadLine());
+            foreach (var f in meccsek)
+            {
+                if (fordulo == f.fordulo)
+                {
+                    Console.WriteLine($"{f.fordulo}.forduló: {f.hazai}-{f.vendeg}: {f.hazaigol}-{f.vendeggol} ({f.hfelido}-{f.vfelido})");
+                }
+            }
         }
     }
 }
