f960f42328be3145517d0394b6d7e1e4ca635713
makyo222 <145643799+makyo222@users.noreply.github.com>
2024-09-10 09:53:28 +0200

    beolvasás kész

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program t = new Program();
+            t.GetData("meccs.txt");
+        }
+        public List<foci> meccsek = new();
+        public void GetData(string fileName)
+        {
+            foreach(var line in File.ReadAllLines(fileName).Skip(1))
+            {
+                int f = int.Parse(line.Split(" ")[0]);
+                int hg = int.Parse(line.Split(" ")[0]);
+                int vg = int.Parse(line.Split(" ")[0]);
+                int hf = int.Parse(line.Split(" ")[0]);
+                int vf = int.Parse(line.Split(" ")[0]);
+                string h = line.Split(" ")[5];
+                string v = line.Split(" ")[6];
+
+                meccsek.Add(new foci(f, hg, vg, hf, vf, h, v));
+            }
+            
         }
     }
 }
--- foci.cs
+++ foci.cs
@@ -8,5 +8,25 @@ namespace ConsoleApp1
 {
     internal class foci
     {
+
+        public int fordulo { get; set; }
+        public int hazaigol { get; set; }
+        public int vendeggol { get;  set; }
+        public int hfelido { get;  set; }
+        public int vfelido { get;  set; }
+        public string hazai { get; set; }
+        public string vendeg { get; set; }
+
+        public foci(int fordulo, int hazaigol, int vendeggol, int hfelido, int vfelido, string hazai, string vendeg)
+        {
+            this.fordulo = fordulo;
+            this.hazaigol = hazaigol;
+            this.vendeggol = vendeggol;
+            this.hfelido = hfelido;
+            this.vfelido = vfelido;
+            this.hazai = hazai;
+            this.vendeg = vendeg;
+        }
+
     }
 }
